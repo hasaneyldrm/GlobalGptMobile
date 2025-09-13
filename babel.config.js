c9528833054911e12e8dbaec75a3bb0ace6a71cd
Hasan Emir Yıldırım <hasanyldrm@hotmail.de>
2025-09-13 20:17:54 +0300

    Babel yapılandırmasına modül çözümleyici eklendi, bağımlılıklar güncellendi ve TypeScript yapılandırması için yol ayarları eklendi. Ayrıca iOS projesinde yeni mimari etkinleştirildi.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,27 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './src',
+          '@/api': './src/api',
+          '@/assets': './src/assets',
+          '@/components': './src/components',
+          '@/config': './src/config',
+          '@/constants': './src/constants',
+          '@/hooks': './src/hooks',
+          '@/navigation': './src/navigation',
+          '@/screens': './src/screens',
+          '@/store': './src/store',
+          '@/theme': './src/theme',
+          '@/types': './src/types',
+          '@/utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
