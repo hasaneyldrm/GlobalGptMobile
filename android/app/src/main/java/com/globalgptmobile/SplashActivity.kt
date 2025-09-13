9b85c0563715bdfb5213b1e32864f064fe836d23
Hasan Emir Yıldırım <hasanyldrm@hotmail.de>
2025-09-13 20:27:24 +0300

    AndroidManifest.xml dosyasında SplashActivity eklendi ve MainActivity'nin yapılandırması güncellendi. Yeni SplashTheme stili eklendi. iOS LaunchScreen.storyboard dosyasında logo görüntüsü eklendi ve arka plan rengi değiştirildi. Uygulama simgeleri için yeni dosyalar eklendi.

---
+++ android/app/src/main/java/com/globalgptmobile/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.globalgptmobile
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        // 2 saniye sonra MainActivity'yi başlat
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
